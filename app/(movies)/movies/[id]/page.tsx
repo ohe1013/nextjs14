abcbc8745b2ec0cec4320c8f6d6117e209ccf802
oh hyun keun <dhgusrms23@naver.com>
2024-02-05 23:30:51 +0900

    why it has somany diff

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,13 +1,12 @@
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 async function getMovie(id) {
-  console.log(`getMovie`);
   const response = await fetch(`${URL}/${id}`);
   return response.json();
 }
 async function getVideo(id) {
-  console.log(`getVideo`);
-  const response = await fetch(`${URL}/${id}/videos`);
-  return response.json();
+  // const response = await fetch(`${URL}/${id}/videos`);
+  // return response.json();
+  throw new Error("something went wrong");
 }
 export default async function SSSD(props) {
   const { id } = props.params;
